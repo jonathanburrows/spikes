0b7c0821ea84ce0102f976fe5db3766dcdd89bb8
jonathanburrows <jonathanburrows@live.com>
2017-05-12 20:27:27 -0700

    added 2 spikes for how to make a layout using google guidelines

---
+++ AngularLayout/src/app/app.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+    showHamburger = false;
+
+    toggleHamburger() {
+        this.showHamburger = !this.showHamburger;
+    }
+}
